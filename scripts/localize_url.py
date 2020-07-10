d540eaa54b99b0d1e96a40155a788ad5d3d2688a
Gede Wahyu <tokekbesi@gmail.com>
2020-07-10 22:25:45 +0700

    add option to overwrite files

--- scripts/localize_url.py
+++ scripts/localize_url.py
@@ -24,22 +24,31 @@ def get_en_links(id_files, path):
     links.add(f[len(path):-3])
   return links
     
-def find_links(id_files, en_links):
+def find_links(id_files, en_links, overwrite=False):
   result = {}
   for idf in id_files:
     bad_links = []
-    with open(idf) as f:
+    with open(idf, "r") as f:
       fread = f.read()
-      for l in en_links:
-        lmd = "(" + l
-        if lmd in fread:
-          bad_links.append(l)
+      f.close()
+      for en_link in en_links:
+        en_link_md = "(" + en_link
+        id_link_md = "(/id" + en_link
+        if en_link_md in fread:
+          bad_links.append(en_link)
+          fread = fread.replace(en_link_md, id_link_md)
+      if overwrite==True:
+        print(overwrite)
+        w = open(idf, "w")
+        w.write(fread)
+        w.close()
     result.update({idf: bad_links})
   return result
 
 @click.command()
 @click.argument("repo")
-def main(repo):
+@click.option("-o","--overwrite",default=False,type=bool,help='overwrite current file to use ID links')
+def main(repo, overwrite):
   """
   find docs that use EN links, although the ID link already localized
 
@@ -53,7 +62,7 @@ def main(repo):
   id_files = get_files(id_path)
   en_links = get_en_links(id_files, id_path_prefix)
 
-  result = find_links(id_files, en_links)
+  result = find_links(id_files, en_links, overwrite)
   pprint(result)
 
 if __name__ == '__main__':
