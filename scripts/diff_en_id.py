1b687fd881e809afdfc7bb9a312681c011b834bb
enchant3dmango <andresitorusz.19@gmail.com>
2025-03-31 20:33:36 +0700

    feat: write en vs id diff to a file

--- scripts/diff_en_id.py
+++ scripts/diff_en_id.py
@@ -4,39 +4,42 @@ from pathlib import Path
 import click
 
 def get_files(path):
-  files = set()
-  for filename in Path(path).rglob('*.md'):
-    files.add(str(filename))
-  
-  return files
+    files = set()
+    for filename in Path(path).rglob('*.md'):
+        files.add(str(filename))
+    return files
 
-def print_diff(source_files, target_files, source_language, target_language):
-  for f in source_files:
-    target_file = str(f).replace('/%s/' % source_language, '/%s/' % target_language)
-    if target_file not in target_files:
-      print(f)
+def write_diff(source_files, target_files, source_language, target_language, output_file):
+    for f in source_files:
+        target_file = str(f).replace(f'/{source_language}/', f'/{target_language}/')
+        if target_file not in target_files:
+            output_file.write(f + '\n')
 
 @click.command()
 @click.argument("repo")
 def main(repo):
-  """
-  Find the diff between EN and ID docs in master branch
+    """
+    Find the diff between EN and ID docs in master branch
 
-  ex:
-    ./diff_en_id.py path/to/website-repo
-  """
-  en_path = "%s/content/en/docs" % (repo)
-  id_path = "%s/content/id/docs" % (repo)
+    ex:
+      ./diff_en_id.py path/to/website-repo
+    """
+    en_path = f"{repo}/content/en/docs"
+    id_path = f"{repo}/content/id/docs"
 
-  en_files = get_files(en_path)
-  id_files = get_files(id_path)
+    en_files = get_files(en_path)
+    id_files = get_files(id_path)
 
-  print('These ID pages must be removed, since no longer exist in latest EN docs:')
-  print_diff(id_files, en_files, 'id', 'en')
+    output_path = Path(__file__).parent / "diff_en_id_result.txt"
 
-  print()
-  print('These EN pages must be translated, since not yet exist in latest ID docs:')
-  print_diff(en_files, id_files, 'en', 'id')
+    with open(output_path, "w") as output_file:
+        output_file.write("These ID pages must be removed, since they no longer exist in latest EN docs:\n")
+        write_diff(id_files, en_files, "id", "en", output_file)
+
+        output_file.write("\nThese EN pages must be translated, since they do not yet exist in latest ID docs:\n")
+        write_diff(en_files, id_files, "en", "id", output_file)
+
+    print(f"Diff results saved to {output_path}. Please check the file for details.")
 
 if __name__ == '__main__':
-  main()
+    main()
